540df480fd3917446bfe93f998a893ea44f70a39
juhan <johann.junti@tptlive.ee>
2023-11-23 13:20:05 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('partials.layout')
+@section('title', 'Home page')
+@section('content')
+    <h1>Hello lavarel</h1>
+    <ul>
+        @for ($i = 0; $i < 10; $i++)
+            <li>{{ $i }}</li>
+        @endfor
+    </ul>
+@endsection
